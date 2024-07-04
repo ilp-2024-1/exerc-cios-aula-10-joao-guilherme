d2dab1a714e153cf81f5427b857768e6251a05f8
JG1511 <joaotsi2024@gmail>
2024-07-03 22:10:28 -0300

    questao02

--- lista01.py
+++ lista01.py
@@ -1,11 +1,18 @@
-#Questão 1.
+# #Questão 1.
 
-a = int(input("Digite um valor: "))
-b = int(input("Digite um valor: "))
+# a = int(input("Digite um valor: "))
+# b = int(input("Digite um valor: "))
 
-soma = a + b
-subtraçao = a - b
-multiplicaçao = a * b
-div = a/b
+# soma = a + b
+# subtraçao = a - b
+# multiplicaçao = a * b
+# div = a/b
 
-print("Os resultados das operaões: ", "Soma:" , soma, "Subtraçao", subtraçao, "multplicaça:", multiplicaçao, "div:", div)
+# print("Os resultados das operaões: ", "Soma:" , soma, "Subtraçao", subtraçao, "multplicaça:", multiplicaçao, "div:", div)
+
+#Questão 2.
+
+c =  float(input("Digite sua temperatura em Celcius: "))
+f = c * (1.8) + 32
+
+print("A temperatura em Fahrenheit:", f)
