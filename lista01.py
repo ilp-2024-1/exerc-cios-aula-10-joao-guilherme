ac35432e0cbaa13e85117bf5689f43aa31c858cc
JG1511 <joaotsi2024@gmail>
2024-07-03 21:44:11 -0300

    qustao 01

---
+++ lista01.py
@@ -0,0 +1,11 @@
+#Questão 1.
+
+a = int(input("Digite um valor: "))
+b = int(input("Digite um valor: "))
+
+soma = a + b
+subtraçao = a - b
+multiplicaçao = a * b
+div = a/b
+
+print("Os resultados das operaões: ", "Soma:" , soma, "Subtraçao", subtraçao, "multplicaça:", multiplicaçao, "div:", div)
