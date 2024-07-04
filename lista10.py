abcaacd24493e31285707885e79b69574c28832a
JG1511 <joaotsi2024@gmail.com>
2024-07-04 09:53:51 -0300

    qt03

---
+++ lista10.py
@@ -0,0 +1,17 @@
+# Questão 01
+
+# n1 = 0
+
+# while( n1 <= 1000):
+#     print(n1)
+#     n1+= 1
+
+#Questão 03
+
+senha =  input("Digite sua senha: ")
+flag =  "carlinhos"
+
+while(senha !=flag):
+    if(senha != flag):
+        senha = input("Digite sua senha novamente: ")
+print("Fim do programa")
