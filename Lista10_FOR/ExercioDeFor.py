c6fc2b6e5e38b855b21fb84f47a9ea952c4c19fd
Robertin76 <robertnobrega14@gmail.com>
2024-07-10 11:23:04 -0300

    questao02

--- Lista10_FOR/ExercioDeFor.py
+++ Lista10_FOR/ExercioDeFor.py
@@ -2,3 +2,11 @@
 
 for i in range(2, 12 +1 , 2):
     print(i, end = (","))
+
+    #Questão02
+
+numero = int(input('Digite um numero:'))
+
+for i in range(1, numero+1):
+    print(i)
+print('Programa encerrado')
