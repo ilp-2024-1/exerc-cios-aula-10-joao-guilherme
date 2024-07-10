745f965f1b46512574e712b3c4c7f56cc39751cc
JG1511 <joaotsi2024@gmail>
2024-07-10 18:54:06 -0300

    questao03

--- lista10While/questaoWhile.py
+++ lista10While/questaoWhile.py
@@ -5,3 +5,13 @@ n1 = 0
 while( n1 <=1000):
     print(n1)
     n1= n1 +1
+   
+# Questão 03
+
+senha =  input("Digite sua senha: ")
+flag =  "carlinhos"
+
+while(senha !=flag):
+    if(senha != flag):
+        senha = input("Digite sua senha novamente: ")
+print("Fim do programa")
