4348eb5fe89ed422b29ac50431d5d6f34221789a
JG1511 <joaotsi2024@gmail>
2024-07-10 19:09:36 -0300

    Questão07

--- lista10While/questaoWhile.py
+++ lista10While/questaoWhile.py
@@ -48,3 +48,24 @@ while(x <= a ):
     fatorial = fatorial * x
     x += 1
 print(fatorial)
+
+# questão 07
+
+a = int(input("Escolha um numero de 1 até 1000: "))
+b = 2
+
+while(b < a):
+    
+    if a == 3 or a == 5:
+        print("Ele é um numero primo")
+        break
+    elif a % b == 0 or a % 3 == 0 or a % 5 == 0:
+        print("Não é primo")
+        break
+    else:
+        print("Ele é um numero primo")
+        
+        break
+       
+    
+print("Fim do programa")
