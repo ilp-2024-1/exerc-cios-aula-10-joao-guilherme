81fb6a7532e82831adf60315ddf78937dd6b92bb
Robertin76 <robertnobrega14@gmail.com>
2024-07-10 11:44:22 -0300

    questao06

--- Lista10_FOR/ExercioDeFor.py
+++ Lista10_FOR/ExercioDeFor.py
@@ -29,3 +29,16 @@ for i in range(num1+1, num2):
     print(i)
 print('Programa encerrado')
 
+#Questão06
+
+num1 = int(input('Digite o valor inicial:'))
+num2 = int(input('Digite o valor final:'))
+num3 = int(input('Digite o incremento:'))
+
+for i in range(num1, num2, num3):  
+    tempf = i*1.8+32
+    print(f"{i}°C = {tempf}°F")
+print('Programa encerrou')
+
+
+
