5184dd518321293ef205e4d53b7eece8c17e6197
JOÃO GUILHERME DE LIMA OLIVEIRA <20241148060011@ifrn.local>
2024-07-10 11:38:01 -0300

    Questão05

--- Lista10_FOR/ExercioDeFor.py
+++ Lista10_FOR/ExercioDeFor.py
@@ -18,4 +18,23 @@ var_2 = int(input("Digite o segundo valor inteiro positivo:"))
 for i in range(var_1, var_2 + 1):
     print(i, end = ",")
 
+#Questão05
 print("Fim do Programa")
+
+var_1 = int(input("Digite o primeiro valor positivo: "))
+var_2 = int(input("Digite o segundo valor positivo: "))
+
+var_soma1 = 0
+var_soma = 0
+
+for i in range(var_1, var_2 + 1):
+    print(i, end= (" , "))
+    if i == 3 or i == 5 or i == 2:
+        var_soma1 += i
+    elif i % 2 == 0 or i % 3 == 0 or i % 5 == 0 :
+        print("Esse numero não é primo: " , i)
+    else:
+       var_soma += i
+var_final = var_soma + var_soma1
+
+print("O valor final é: ", var_final -1)
