05e8ca3a4e6b8972a88390cb8b3d9457753efef7
Robertin76 <robertnobrega14@gmail.com>
2024-07-10 11:26:32 -0300

    questao04

--- Lista10_FOR/ExercioDeFor.py
+++ Lista10_FOR/ExercioDeFor.py
@@ -10,3 +10,12 @@ numero = int(input('Digite um numero:'))
 for i in range(1, numero+1):
     print(i)
 print('Programa encerrado')
+
+#Questão04
+
+num1 = int(input('Digite o numero:'))
+num2 = int(input('Digite o segundo numero:'))
+
+for i in range(num1+1, num2):
+    print(i)
+print('Programa encerrado')
