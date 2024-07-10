44351f34f78ab25f5e126fa5520482ccbbb25209
JOÃO GUILHERME DE LIMA OLIVEIRA <20241148060011@ifrn.local>
2024-07-10 11:30:09 -0300

    Questão03

--- Lista10_FOR/ExercioDeFor.py
+++ Lista10_FOR/ExercioDeFor.py
@@ -10,3 +10,12 @@ numero = int(input('Digite um numero:'))
 for i in range(1, numero+1):
     print(i)
 print('Programa encerrado')
+
+#Questão 03
+var_1 = int(input("Digite o pireimeiro valor inteiro positivo: "))
+var_2 = int(input("Digite o segundo valor inteiro positivo:"))
+
+for i in range(var_1, var_2 + 1):
+    print(i, end = ",")
+
+print("Fim do Programa")
